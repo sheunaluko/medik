494cd95b68eac1f8bc28b55cabe54cf27ea4f6b0
Sheun Aluko <alukosheun@gmail.com>
2020-04-13 01:01:16 -0700

    +backend

---
+++ bin/backend.js
@@ -0,0 +1,4 @@
+console.log("This file has not been implemented yet :)") 
+console.log("Maybe you can implement it now?") 
+
+
--- src/typescript/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    roots: ['dist'],
-    //transform: {
-    //  '^.+\\.jsx?$': '/usr/local/bin/ts-jest',
-    //},
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  }
