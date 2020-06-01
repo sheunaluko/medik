68701171a9cda34667c1efd38be042f2bdc2487e
Gustavo Chavez <gustavochavez01@gmail.com>
2020-05-31 21:58:58 -0700

    added the search function

--- src/react_js/medik_web_app/src/Components/SearchComponent.jsx
+++ src/react_js/medik_web_app/src/Components/SearchComponent.jsx
@@ -1,38 +1,77 @@
 import React from 'react'
-import { Dropdown, Button,Grid } from 'semantic-ui-react'
+import { Dropdown, Button,Grid, Segment, Loader, Dimmer, Image } from 'semantic-ui-react'
+import stateOptions from './stateOptions'
+//
+// const stateOptions = [
+//     {
+//         key:1,
+//         text: "Cough",
+//         value: "Cough"
+//     },
+//     {
+//         key: 2,
+//         text: "Fever",
+//         value: "Fever"
+//     }
+// ]
 
-const stateOptions = [
-    {
-        key:1,
-        text: "Cough",
-        value: "Cough"
-    },
-    {
-        key: 2,
-        text: "Fever",
-        value: "Fever"
+
+
+
+class DropdownExampleMultipleSearchSelection  extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading : false,
+            selected:[],
+
+        }
     }
-]
 
-const DropdownExampleMultipleSearchSelection = () => (
-    <Grid fluid>
-        <Grid.Row >
-            <Grid.Column width={13}>
-    <Dropdown
-        placeholder='Sign or Symptom'
-        fluid
-        multiple
-        search
-        selection
-        options={stateOptions}
-    />
-            </Grid.Column>
-        <Grid.Column width={3}>
-                <Button fluid size='big'> Search</Button>
+    prepareSearch= () => {
+        console.log(this.state.selected)
+        this.props.sendSearch(
+            this.state.selected
+        )
+    };
 
-        </Grid.Column>
-        </Grid.Row>
-    </Grid>
-)
+    handleChange = (e, { value }) => this.setState({ selected: value })
+
+    render() {
+
+        if (this.props.isLoading){
+            return <Segment>
+                <Dimmer active>
+                    <Loader />
+                </Dimmer>
+
+                <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />
+            </Segment>
+        }
+
+
+        return <Grid fluid>
+            <Grid.Row >
+                <Grid.Column width={13}>
+                    <Dropdown
+                        placeholder='Sign or Symptom'
+                        fluid
+                        multiple
+                        search
+                        selection
+                        options={stateOptions}
+                        //onChange={this.handleChange}
+
+                    />
+                </Grid.Column>
+                <Grid.Column width={3}>
+                    <Button fluid size='big' onClick={this.prepareSearch}> Search</Button>
+
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    }
+
+}
 
 export default DropdownExampleMultipleSearchSelection
--- src/react_js/medik_web_app/src/Components/SearchWidget.jsx
+++ src/react_js/medik_web_app/src/Components/SearchWidget.jsx
@@ -4,13 +4,37 @@ import ResultsTableComponent from "./ResultsTableComponent"
 import ResultsAccordionComponent from "./ResultsAccordionComponent"
 import {Container} from 'semantic-ui-react'
 
-function SearchWidget() {
-    return (
-        <Container style={{margin: "10px"}} >
-            <SearchComponent />
-            <ResultsAccordionComponent />
-        </Container>
-    );
+class SearchWidget extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading:false
+        }
+    }
+
+    sendSearch = (selected) => {
+
+        this.setState({isLoading:true})
+
+
+        setTimeout(function() { //Start the timer
+            this.setState({isLoading: false}) //After 1 second, set render to true
+        }.bind(this), 3000)
+    }
+
+    render() {
+        return (
+            <Container style={{margin: "10px"}}>
+                < SearchComponent
+                    isLoading={this.state.isLoading}
+                    sendSearch={this.sendSearch}
+
+                />
+                <ResultsAccordionComponent/>
+            </Container>
+        );
+    }
 }
 
 
